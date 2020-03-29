fbbaeb8765479bdf62c419dfe83794d7f5c20617
Biplov Pokhrel <pbplop@gmail.com>
2020-03-29 09:27:28 +0530

    Leanrt upto wtforms, database next

---
+++ forms.py
@@ -0,0 +1,68 @@
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, Form, RadioField, SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.fields.html5 import DateField
+
+#so obviously imported Flaskform
+#imported RecaptchaField from flask_wtf because it is somewhat different from ususal wtforms fields
+#imported fields
+#imported validators
+#imported the Datefield from html5, this shows the drop down thing 
+
+
+class signupform(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+
+    #StringField --> just a field for regular string
+    # DataRequired() --> cant leave the field empty
+    #Length(min=x, max=y) --> Data cannot be less than x and greater than y
+
+
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    
+    #Email() --> checks if the input is correct email format
+
+    birthdate = DateField("Date of Birth", format='%Y-%m-%d', validators=[DataRequired()])
+
+    #DateField -> for the date drop down and we set this sepecific format because the date no matter the format we use in the UI gets rendered in this format
+
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    #PasswordField --> Bulletifies the characters
+
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+    #EqualTo() --> checks if the provided field is equal to the field mentioned withis this
+
+    recaptcha = RecaptchaField()
+
+    #RecaptchaField() --> just enables the recaptcha thing at this place, recaptcha is a little different as it requires keys to be put in the main file, the keys we recieve from the recaptcha providing host.
+                # I used GOOGLE recaptcha services
+
+    submit = SubmitField('Sign Up')
+    
+    #Genertaes a submit button which acts as post
+
+    radios = RadioField('Gender', default='3', choices= [('1', 'Male'), ('2', 'Female'), ('3', 'Confidential')])
+
+    #RadioField --> basiaacally the choose from options
+            #--> in our form helpers we have used these such that the first value in choices is the id and second is the label
+
+    selects = SelectField("age group", choices=[
+        ('1', '0-10'), ('2', '10-25'), ('3', '35-60'), ('4', '50+')
+    ])
+
+    #basically a drop down seclection
+    #acts as a pop up radio field thing but works with the form helper of regular things
+
+
+class signinform(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+
+    #A True or flase thing
+
+    submit = SubmitField('Sign In')
---
+++ main.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import signupform, signinform
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+#imported all things
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '356c2f03e9fd4d3c4287a1366c97fa24'
+#csrf thing
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LcBHOQUAAAAADf4u1nvZ7aAaR3a8G_9_u7oobAB'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LcBHOQUAAAAAKUgkjvxe0qUedGHotX8aP6MagvZ'
+#recaptcha things
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+#gives location of the database file, or basically creates a database file when we do create all.
+db = SQLAlchemy(app)
+#SQLAlchemy thigns
+
+
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(120), unique=True, nullable=False)
+
+    email = db.Column(db.String(20), unique=True, nullable=False)
+
+    image_file = db.Column(db.String(20), unique=False, nullable=False, default='default.jpeg')
+
+    password = db.Column(db.String(60), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+#Basically a database thing
+#id gives a id, and primary key set to true provides a key even if not input.
+#username is sth we will use in the database obviously and also i have to learn how to connect to classes inside a database from the Corey Schafer videos.
+#others are similar
+#the classes inside are important
+#primary_key gives key
+#unique should be unique
+#nullable cant be left empty
+#default gives provided value in case data not given
+
+
+#thre __repr__(self):
+#this thing is required to give out the values mentioned in it, whatever is called is then used in the datbase
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", title_given="Home")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title_given="About")
+
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = signupform()
+    if form.validate_on_submit():
+        flash('sign up successful', 'success')
+        return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + str(form.birthdate.data) + form.radios.data + form.selects.data + form.password.data + '</h1>'
+
+    return render_template("signup.html", title_given="SignUp", form=form)
+
+
+@app.route("/signin", methods=['GET', 'POST'])
+def signin():
+    form = signinform()
+    if form.validate_on_submit():
+        flash('sign in successful', 'success')
+        return '<h1>' + form.email.data + ' '+form.password.data + '</h1>'
+
+    return render_template("signin.html", title_given="SignIn", form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
